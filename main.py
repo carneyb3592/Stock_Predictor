ecded74b8de2359e9521f902995d4aa4a8519ea5
Ben Carney <ben.carney3@gmail.com>
2021-04-14 16:17:00 -0700

    Can parse through all comments in a post now

--- main.py
+++ main.py
@@ -9,24 +9,24 @@ reddit = praw.Reddit(client_id="khD2wxnPocaxjA",
 api = PushshiftAPI(reddit)
 counter = 0
 
-start_time = int((dt.datetime.today() - dt.timedelta(days=1)).timestamp())
-end_time = int(dt.datetime.today().timestamp())
-#posts = list(api.search_submissions(after = start_time,before = end_time,subreddit = 'wallstreetbets', filter = ['title'], limit = None))
-comments = list(api.search_comments(after = start_time,before = end_time,subreddit = 'wallstreetbets', filter = ['title'], limit = None))
-counter = 0
-for post in comments:
-    print("")
-    print(post.body)
-    print("")
-    counter += 1
-print(counter)
-# for submission in reddit.subreddit('wallstreetbets').search("today",limit = None):
-#     print(submission.title)
-#     print(" ")
-#     counter += 1
-#     #submission.comments.replace_more(limit = 1)
-#     #for comment in submission.comments.list():
-#     #    print(comment.body)
+#start_time = int((dt.datetime.today()- dt.timedelta(days=1)).timestamp())
+#end_time = int((dt.datetime.today() - dt.timedelta(days=1)).timestamp())
+#posts = list(api.search_submissions(after = start_time,subreddit = 'wallstreetbets', filter = ['title'], limit = None))
+#comments = list(api.search_comments(after = start_time,subreddit = 'wallstreetbets', filter = ['title'], limit = None))
+#counter = 0
+#for post in comments:
+#    print("")
+#    print(post.title)
+#    print("")
+#    counter += 1
+#print(counter)
+for submission in reddit.subreddit('wallstreetbets').search('flair:"Daily Discussion"',limit = 1,syntax='lucene'):
+    print(submission.title)
+    print(" ")
+    submission.comments.replace_more(limit = None)
+    for comment in submission.comments.list():
+        counter += 1
+        #print(comment.body)
 # #tickers = pd.read_csv('stock_tickers.csv')
 # #print(tickers)
-# print(counter)
+print(counter)
