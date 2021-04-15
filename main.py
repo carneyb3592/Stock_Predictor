5edb252b2d4cb9c1794eabd512d484f314cb0551
Ben Carney <ben.carney3@gmail.com>
2021-04-15 00:23:24 -0700

    Finished reddit scraping

--- main.py
+++ main.py
@@ -1,32 +1,32 @@
 import praw
-from psaw import PushshiftAPI
 import pandas as pd
 import re
 import datetime as dt
 reddit = praw.Reddit(client_id="khD2wxnPocaxjA",
                      client_secret="ipAIgBiv7KUux0hqjYZZixoEm03N-g",  
                      user_agent="ben_scrape")
-api = PushshiftAPI(reddit)
+file = pd.read_csv('stock_tickers.csv')
+tickers = file.iloc[:,0:2]
+#tickers["Name"] = tickers["Name"].series.str.split(' ')[0]
+#print(tickers)
 counter = 0
-
-#start_time = int((dt.datetime.today()- dt.timedelta(days=1)).timestamp())
-#end_time = int((dt.datetime.today() - dt.timedelta(days=1)).timestamp())
-#posts = list(api.search_submissions(after = start_time,subreddit = 'wallstreetbets', filter = ['title'], limit = None))
-#comments = list(api.search_comments(after = start_time,subreddit = 'wallstreetbets', filter = ['title'], limit = None))
-#counter = 0
-#for post in comments:
-#    print("")
-#    print(post.title)
-#    print("")
-#    counter += 1
-#print(counter)
+tickerDict = {}
+regex = re.compile('[^a-zA-Z ]')
 for submission in reddit.subreddit('wallstreetbets').search('flair:"Daily Discussion"',limit = 1,syntax='lucene'):
-    print(submission.title)
-    print(" ")
-    submission.comments.replace_more(limit = None)
-    for comment in submission.comments.list():
-        counter += 1
-        #print(comment.body)
-# #tickers = pd.read_csv('stock_tickers.csv')
-# #print(tickers)
-print(counter)
+   submission.comments.replace_more(limit = None)
+   for comment in submission.comments.list():
+       
+       splitComment = regex.sub('',str(comment.body))
+       splitComment = splitComment.split(' ')
+       for word in splitComment:
+            if word in tickerDict:
+                tickerDict[word] += 1
+            else:
+                tickerDict[word] = 1
+tickerCounter_df = pd.DataFrame.from_dict(list(tickerDict.items())).rename(columns = {0:"Symbol",1:"Frequency"})
+final_ticker_df = pd.merge(tickers,tickerCounter_df, on ="Symbol")
+final_ticker_df = final_ticker_df.sort_values(by = "Frequency", ascending = False, ignore_index = True)
+print(final_ticker_df)
+       #print(comment.body)
+#print(tickers)
+#print(counter)
