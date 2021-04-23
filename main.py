20a774009e854f8453192c7622c4971e2dd6b428
Ben Carney <ben.carney3@gmail.com>
2021-04-23 02:52:22 -0700

    Fixed some code that ignored Stock Ticker

--- main.py
+++ main.py
@@ -2,22 +2,29 @@ import praw
 import pandas as pd
 import re
 import datetime as dt
+from datetime import date
 reddit = praw.Reddit(client_id="khD2wxnPocaxjA",
                      client_secret="ipAIgBiv7KUux0hqjYZZixoEm03N-g",  
                      user_agent="ben_scrape")
 file = pd.read_csv('stock_tickers.csv')
 tickers = file.iloc[:,0:2]
-tickers["Name"] = tickers["Name"].str.split(' ').str[0].str.lower()
-#print(tickers)
 
+#Sets up name to be compared in text
+
+#tickers["Name"] = tickers["Name"].str.split(' ').str[0].str.lower()
+#print(tickers)
 counter = 0
 tickerDict = {}
+
+#Filter for only letters
 regex = re.compile('[^a-zA-Z ]')
 banned_words = ['on','go','next','fat','love','main','nice','the','up','new','big','first','two','old','rh','red','whole','under','home','dollar','ready','world','bank',
-                'virign','national','gee']
+                'virign','national','gee','hope','second','rocket','safe','general','a','k','m','dd']
 
+
+#Searches Daily Discussions for stock tickers
 for submission in reddit.subreddit('wallstreetbets').search('flair:"Daily Discussion"',sort = 'new',limit = 2,syntax='lucene'):
-   
+
    submission.comments.replace_more(limit = None)
    
    for comment in submission.comments.list():
@@ -34,6 +41,7 @@ for submission in reddit.subreddit('wallstreetbets').search('flair:"Daily Discus
             else:
                 tickerDict[word.lower()] = 1
                 tickerDict[word] = 1
+#Searches all hot posts for stock tickers
 for submission in reddit.subreddit('wallstreetbets').hot(limit = None):
     splitTitle = regex.sub('',str(submission.title))
     splitTitle = splitTitle.split(' ')
@@ -49,17 +57,22 @@ for submission in reddit.subreddit('wallstreetbets').hot(limit = None):
         else:
             tickerDict[word.lower()] = 1
             tickerDict[word] = 1
+
+#Creates Datafram for the tickers found and
 tickerCounter_df = pd.DataFrame.from_dict(list(tickerDict.items())).rename(columns = {0:"Symbol",1:"Frequency"})
 final_ticker_df = pd.merge(tickers,tickerCounter_df, on ="Symbol")
 
-tickerCounter_df2 = pd.DataFrame.from_dict(list(tickerDict.items())).rename(columns = {0:"Name",1:"Frequency2"})
-final_ticker_df = pd.merge(final_ticker_df,tickerCounter_df2)
 
-final_ticker_df['Frequency'] = final_ticker_df['Frequency'] + final_ticker_df['Frequency2']
-final_ticker_df = final_ticker_df.drop(['Frequency2'],axis = 1)
+#tickerCounter_df2 = pd.DataFrame.from_dict(list(tickerDict.items())).rename(columns = {0:"Name",1:"Frequency2"})
+#final_ticker_df = pd.merge(final_ticker_df,tickerCounter_df2, on = "Name")
+
+#final_ticker_df['Frequency'] = final_ticker_df['Frequency'] + final_ticker_df['Frequency2']
+#final_ticker_df = final_ticker_df.drop(['Frequency2'],axis = 1)
 final_ticker_df = final_ticker_df.sort_values(by = "Frequency", ascending = False, ignore_index = True)
 
-final_ticker_df.to_csv(r'C:\Users\djntr\StockPredictor\Stock_Predictor\df.csv',index=False)
+
+filename = '\\' + str(date.today()) + '_stocks.csv'
+final_ticker_df.to_csv(r'C:\Users\djntr\StockPredictor\Stock_Predictor' + filename ,index=False)
 print(final_ticker_df.head(20))
        #print(comment.body)
 #print(tickers)
